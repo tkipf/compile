822b367b357eeaa137839cb1e6b22ed46961024f
Thomas Kipf <thomas.kipf@gmail.com>
2019-01-22 11:04:34 +0100

    Update docstring

--- modules.py
+++ modules.py
@@ -6,7 +6,7 @@ import utils
 
 
 class CompILE(nn.Module):
-    """CompILE reference implementation (non-batched, single sample only).
+    """CompILE example implementation (non-batched, single sample only).
 
     Args:
         input_dim: Dictionary size of embeddings.
