319632f0c4e5071f1f62f69dba7b9276d626a56d
tkipf <thomas.kipf@gmail.com>
2019-05-09 23:12:06 +0200

    Remove redundant device placement

--- modules.py
+++ modules.py
@@ -182,8 +182,6 @@ class CompILE(nn.Module):
         probs_b = F.softmax(all_b['logits'][0], dim=-1)
         log_prior_b = utils.poisson_categorical_log_prior(
             probs_b.size(1), self.prior_rate, device=inputs.device)
-        if inputs.is_cuda:
-            log_prior_b = log_prior_b.cuda()
         kl_b = self.max_num_segments * utils.kl_categorical(
             probs_b[:, 1:], log_prior_b[:, 1:]).mean(0)
 
